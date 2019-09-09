55996d66e0b938245ff2dcd3121f13ad5b9573f6
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-09 20:55:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,14 +18,15 @@ end
 
  
 
-def get_japanese_emoticon(file_path, emoticon)
-  emoticons = load_library(file_path) 
- emoticons["get_emoticon"][emoticon]
+def get_japanese_emoticon(path, emoticon)
+  emoticons = load_library(path) #call load_library
+  result = emoticons["get_emoticon"][emoticon]
   if result
     result
   else
     "Sorry, that emoticon was not found"
   end
+end
 
 end
 
