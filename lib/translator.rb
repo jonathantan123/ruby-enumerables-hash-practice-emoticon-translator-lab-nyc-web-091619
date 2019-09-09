58a9402b0a3e9043ee43b65c4e8f81ce9d8d3ed2
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-09 21:01:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,6 +28,13 @@ def get_japanese_emoticon(path, emoticon)
   end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning (path, emoticon)
+ emoticons = load_library(path) 
+  result = emoticons["get_meaning"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
 end
