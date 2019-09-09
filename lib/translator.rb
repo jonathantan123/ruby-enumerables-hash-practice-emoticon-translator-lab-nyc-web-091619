380d5204f16d37c3a6111bcbb8641f409105fe6f
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-09 19:51:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@
 require 'yaml'
 
 def load_library (file_path)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticons = YAML.load_file('emoticons.yml')
 
    emoticon_lib = {"get_meaning"  => {},
                   "get_emoticon" => {} }
